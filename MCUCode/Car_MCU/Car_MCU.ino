035694fc04a744004a0d82395c9d012d62035355
wenhuix <xwh1@mail.ustc.edu.cn>
2014-03-12 20:01:19 +0800

    add direction reverse protect

--- MCUCode/Car_MCU/Car_MCU.ino
+++ MCUCode/Car_MCU/Car_MCU.ino
@@ -1,73 +1,82 @@
 #include <Servo.h> 
 #include <Wire.h>
 
-#define TURN_LEFT_MAX  117
-#define TURN_RIGHT_MIN 60
-#define INI_SPEED 1465
-#define INI_ANGLE 1500
+#define TURN_LEFT_MAX  115
+#define TURN_RIGHT_MIN 65
+#define MID_DRIVE 1500
+#define MID_ANGLE 1500
 
 Servo angle;//Rotation angle servo
 Servo drive;//Driver motor
 
 boolean flag = false;
-int lastSpeed = 0;
+int lastDrive = MID_DRIVE;
 char command[2] = {0};  // command[0] = speed; command[1] = angle
 
 //20 triggers per cycle, the car can march about 37cm
 volatile int triggers = 0;
-//volatile int speed;
+int speed;
 
 void setup() 
 { 
   Serial.begin(9600);//Serial port
-  attachInterrupt(0, extInterrupt0, CHANGE);
+
+  angle.attach(9);
+  drive.attach(10);
   initializeServo();
+  
+  attachInterrupt(0, extInterrupt0, CHANGE);
 }
 
 void loop() {
-  
-  controlCar();
+  int drive = MID_DRIVE + 2 * command[0];
+  int angle = 90 + command[1]/2;
+  controlCar(drive, angle);
 } 
 
+///////////////////////////////////////
+// send rotation angle and drive value
+void controlCar(int drive, int angle)
+{
+  //printInfo(speed, direction);
+  setDrive(drive);
+  setAngle(angle);
+}
+
+////////////////////////////////////////
 //interrupt function
+//used to calculate speed
 void extInterrupt0(){
   triggers++;
 }
 
+////////////////////////////////////////
+//receive serial prot data
 void serialEvent(){
   if(Serial.available()>=3)
   {
     char temp[3] = {0};
-    Serial.readBytes(temp, 3);
-    Serial.print(" command0: ");
-    Serial.print(temp[0], DEC);
-    Serial.print(" command1: ");
-    Serial.println(temp[1], DEC);
     if(temp[0] == 111){
       command[0] = temp[1];
       command[1] = temp[2];
     }
+    //discard the rest of the data
     while(Serial.read());
   }
 }
 
+////////////////////////////////////////
+// Initialize the two servo
 void initializeServo(){
-  angle.attach(9);
-  drive.attach(10);
-  lastSpeed = INI_SPEED;
-  //Initialize the drive motor
-  drive.writeMicroseconds(INI_SPEED);     // set servo to mid-point
-  delay(500);
-  //Initialize the direction servo
-  angle.writeMicroseconds(INI_ANGLE);  // set servo to mid-point
-  delay(500);
+  drive.writeMicroseconds(MID_DRIVE);
+  delay(500); //DON'T REMOVE IT!!!!
+  angle.writeMicroseconds(MID_ANGLE);
+  delay(500); //DON'T REMOVE IT!!!!
 }
 
-char convertByteToChar(byte n) {
-  if (n < 128) return n;
-  return n - 256;
-}
-
+/////////////////////////////////////////
+// set rotation angle
+// between 0 and 180 degrees
 void setAngle(int val)
 {
   if(val>TURN_LEFT_MAX){
@@ -78,35 +87,64 @@ void setAngle(int val)
   angle.write(val);
 }
 
+/////////////////////////////////////////
+// Set drive value
+/* On standard servos 1000us is fully counter-clockwise, 
+* 2000us is fully clockwise, and 1500us is in the middle.
+*
+* In this DC motor controller, value < 1500 is backward
+* value > 1500 is forward. 
+* the power in proportion to |value-1500|
+*
+* I don't want to add the following comment, but there is no choice.
+* It's depends on DC motor comtorller.
+* 1) the value should add slowly; 
+* 2) if lastDrive < 1500 and val > 1500 this time, it should be
+*    wait for a second (depends on DC motor controller)
+*/
 void setDrive(int val)
 {
-  int residual = lastSpeed - val;
-  if(residual > 0){
-    for(int i=0; i<residual; i++)
-    {
-      drive.writeMicroseconds(lastSpeed - i);
-      delay(1);
-    }
+  boolean isreverse = false;
+  int mode;
+  int r = val - lastDrive;
+  if((lastDrive - MID_DRIVE) * (val - MID_DRIVE) < 0)
+    isreverse = true;
+  
+  if(!isreverse && r >= 0)                mode = 1; //speed up(forward); slow down(backward)
+  else if(!isreverse && r < 0)            mode = 2; //slow down(forward); speed up(backward)
+  else if(isreverse && val >= MID_DRIVE)  mode = 3; //from backword to forward
+  else(isreverse && val < MID_DREIVE)     mode = 4; //from forward to backword
+  
+  switch(mode){
+    case 1:
+      for(int i=0; i<r; i++){  
+        drive.writeMicroseconds(lastDrive + i);
+        delay(1); 
+      }
+      break;
+    case 2:
+      for(int i=0; i<-r; i++){  
+        drive.writeMicroseconds(lastDrive - i);
+        delay(1); 
+      }
+      break;
+    case 3:
+      for(int i=0; i<val - MID_DRIVE; i++){
+        drive.writeMicroseconds(MID_DRIVE + i);
+        delay(1); 
+      }
+      break;
+    case 4:
+      for(int i=0; i<MID_DRIVE - val; i++){
+        drive.writeMicroseconds(MID_DRIVE - i);
+        delay(1); 
+      }
   }
-  if(residual < 0){ 
-    for(int i=0; i<-residual; i++)
-    {
-      drive.writeMicroseconds(lastSpeed + i);
-      delay(1);
-    }
-  }
-  lastSpeed = val;
+  //keep the last value
+  lastDrive = val;
 }
 
-void controlCar()
-{
-  int drive = INI_SPEED + 2 * command[0];
-  int angle = 90 + command[1]/2;
 
-  //printInfo(speed, direction);
-  setDrive(drive);
-  setAngle(angle);
-}
 
 void printInfo(int speed, int direction){
   Serial.print("Speed: ");
